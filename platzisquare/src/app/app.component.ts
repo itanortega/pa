caf2d3902dfbb48ccc500a7938565725703b637d
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-01-09 08:57:49 -0500

    Directivas en detalle

--- platzisquare/src/app/app.component.ts
+++ platzisquare/src/app/app.component.ts
@@ -8,9 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PlatziSquere';
   lugares:any = [
-    {nombre: 'Floreria la gardenia', active: true},
-    {nombre: 'Donas la pasadita', active: false},
-    {nombre: 'Veterinaria huellitas felices', active: true}
+    {nombre: 'Floreria la gardenia', active: true, cercania: 1, distancia: 30, plan: 'pagado'},
+    {nombre: 'Donas la pasadita', active: false, cercania: 2, distancia: 60, plan: 'gratuito'},
+    {nombre: 'Veterinaria huellitas felices', active: true, cercania: 2, distancia: 60, plan: 'pagado'},
+    {nombre: 'Floreria la gardenia2', active: true, cercania: 3, distancia: 90, plan: 'gratuito'},
+    {nombre: 'Donas la pasadita2', active: false, cercania: 1, distancia: 30, plan: 'pagado'},
+    {nombre: 'Veterinaria huellitas felices2', active: true, cercania: 2, distancia: 60, plan: 'pagado'},
   ];
   lat:number = 1.2060377;
   lng:number = -77.2629534;
